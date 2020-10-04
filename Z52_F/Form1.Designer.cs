4139b2b319fdf57713130abe5ccb68e7ee393a89
snoy77 <61237537+snoy77@users.noreply.github.com>
2020-10-04 12:35:12 +0500

    create first interface

--- Z52_F/Form1.Designer.cs
+++ Z52_F/Form1.Designer.cs
@@ -29,33 +29,139 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox_StartPath = new System.Windows.Forms.TextBox();
+            this.textBox_EndPath = new System.Windows.Forms.TextBox();
+            this.textBox_NewExcelName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(366, 126);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(234, 109);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 95);
+            this.button1.Size = new System.Drawing.Size(164, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Начать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_StartPath
+            // 
+            this.textBox_StartPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_StartPath.Location = new System.Drawing.Point(170, 12);
+            this.textBox_StartPath.Name = "textBox_StartPath";
+            this.textBox_StartPath.Size = new System.Drawing.Size(529, 20);
+            this.textBox_StartPath.TabIndex = 2;
+            this.textBox_StartPath.TextChanged += new System.EventHandler(this.textBox_StartPath_TextChanged);
+            this.textBox_StartPath.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox_StartPath_MouseDoubleClick);
+            // 
+            // textBox_EndPath
+            // 
+            this.textBox_EndPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_EndPath.Location = new System.Drawing.Point(170, 38);
+            this.textBox_EndPath.Name = "textBox_EndPath";
+            this.textBox_EndPath.Size = new System.Drawing.Size(529, 20);
+            this.textBox_EndPath.TabIndex = 3;
+            this.textBox_EndPath.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox_EndPath_MouseDoubleClick);
+            // 
+            // textBox_NewExcelName
+            // 
+            this.textBox_NewExcelName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_NewExcelName.Location = new System.Drawing.Point(170, 64);
+            this.textBox_NewExcelName.Name = "textBox_NewExcelName";
+            this.textBox_NewExcelName.Size = new System.Drawing.Size(529, 20);
+            this.textBox_NewExcelName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Папка с ТУ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Папка с отмеченными ТУ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Имя создаваемой таблицы";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(201, 439);
+            this.checkedListBox1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(213, 476);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Word файлы в выбранном пути";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 578);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_NewExcelName);
+            this.Controls.Add(this.textBox_EndPath);
+            this.Controls.Add(this.textBox_StartPath);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_StartPath;
+        private System.Windows.Forms.TextBox textBox_EndPath;
+        private System.Windows.Forms.TextBox textBox_NewExcelName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
